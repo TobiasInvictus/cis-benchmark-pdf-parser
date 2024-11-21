12caf5d5d69233d610a13fa044af2c216d94e251
Tobias <tobias.seijsener@invictus-ir.com>
2024-11-21 16:28:39 +0100

    feat: finally automate this

--- json_check_enricher.py
+++ json_check_enricher.py
@@ -1,23 +1,68 @@
 import csv
 import json
 
-test = {}
-test2 = []
-test3 = {}
 
-with open('TODO.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',')
-    for row in reader:
-        test[row["Source Number"]] = row.get("Check Number", "")  
+files = ['M365.json', 'Azure.json']
 
-with open ('cis.json', 'r') as jsonfile:
-    d = json.load(jsonfile)
-    for x in d:
-        check = test.get(x["Rule_number"], "")
-        for source,check in test.items():
-            if source == x["Rule_number"]:
-                x["Check_number"] = check 
-                test2.append(x)
+def main():
+    merge()
+    enrich()
 
-with open('results.json', 'w+') as jsonwrite:
-    jsonwrite.write(json.dumps(test2))
+
+'''Merges the M365.json and Azure.json made with the json_cis_pdf_parser.py'''
+def merge():
+    result = list()
+
+    for file in files:
+        with open(file, 'r') as fi:
+            jsonData = json.load(fi)
+               
+        for line in jsonData: 
+            if file == "M365.json":
+                line.update({'pdf': 'm365'})
+            elif file == "Azure.json":
+                line.update({'pdf': 'azure'})
+            else:
+                exit
+            
+            '''This little gem does some match replace to fix how the dict ends up looking'''
+            for key, value in line.items():
+                if type(value) is dict:
+                    for k, v in value.items():
+                        value[k] = v.replace(": \n1", "1")
+
+
+        result.extend(jsonData)
+
+    with open('output.json', 'w') as fo:
+        json.dump(result, fo)
+
+
+'''The todo's to link the CIS item to the To Do'''
+def enrich():
+    lookup = dict()
+    result = list()
+
+    with open('TODO.csv', newline='') as fc:
+        reader = csv.DictReader(fc, delimiter=',')
+        for row in reader:
+            lookup[row["Source Number"]] = row.get("Check Number", "")  
+
+    with open ('output.json', 'r') as fi:
+        cisItems = json.load(fi)
+        for item in cisItems:
+            check = lookup.get(item["Rule_number"], "")
+            for source,check in lookup.items():
+                if source == item["Rule_number"]:
+                    if check.startswith("A") and item["pdf"] == "azure":
+                        item["Check_number"] = check 
+                        result.append(item)
+                    elif check.startswith("M") and item["pdf"] == "m365":
+                        item["Check_number"] = check 
+                        result.append(item)
+
+    with open('results.json', 'w+') as fw:
+        fw.write(json.dumps(result))
+
+
+main()
